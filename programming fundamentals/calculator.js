2bee53929727ad161fcd15a0d7c0b5503ef8744e
haleynrosi <haleynfisher018@gmail.com>
2022-10-25 20:23:19 -0700

    Thousandth commit

---
+++ programming fundamentals/calculator.js
@@ -0,0 +1,36 @@
+
+
+function addnumbers(a,b) {
+    let c = a + b; 
+    console.log(c)
+}
+
+addnumbers(6,5);
+
+function simplecalculator(a,b,o){
+   if (o == '+') {
+      let c = a +b;
+      console.log(c);
+   } else if (o == '-') {
+     let c = a-b;
+     console.log(c);
+   }
+   else if (o == '*') {
+    let c = a * b;
+    console.log(c);
+   }
+   else {
+    let c = "not a thing my guy";
+    console.log(c);
+   }
+}
+
+simplecalculator(4,5,'+')
+simplecalculator(2,3,'*')
+
+
+
+
+
+
+
---
+++ programming fundamentals/myfirstprogram.js
@@ -0,0 +1,5 @@
+let age = 20;
+let newname = "Haley";
+console.log(age);
+console.log(newname);
+
